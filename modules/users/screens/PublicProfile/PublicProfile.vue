8945e5440fd9c9b48b89bf806f1d80433274c070
Rythelle <rythelle@bgcbrasil.com.br>
2024-04-06 10:26:48 -0300

    creating PublicProfile

--- modules/users/screens/PublicProfile/PublicProfile.vue
+++ modules/users/screens/PublicProfile/PublicProfile.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
 import PublicHeadLine from '@/modules/users/components/PublicHeadLine/PublicHeadLine.vue'
+import WidgetGroup from '@/modules/reports/components/Widget/Group/Group.vue'
+import WidgetCondensed from '@/modules/reports/components/Widget/Condensed/Condensed.vue'
 </script>
 
 <template>
   <PublicHeadLine />
+
+  <WidgetGroup>
+    <WidgetCondensed :value="10" label="Gists total" />
+    <WidgetCondensed :value="5" label="Gists gratuitos" />
+    <WidgetCondensed :value="5" label="Gists pagos" />
+  </WidgetGroup>
 </template>
