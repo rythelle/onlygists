9e66007ab1038dc10e0587c70fa7d67e7723f24c
Rythelle <rythelle20@hotmail.com>
2024-04-08 19:53:44 -0300

    feat: creating Loader component

--- modules/users/screens/PublicProfile/PublicProfile.vue
+++ modules/users/screens/PublicProfile/PublicProfile.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import PublicHeadLine from '@/modules/users/components/PublicHeadLine/PublicHeadLine.vue'
 import WidgetGroup from '@/modules/reports/components/Widget/Group/Group.vue'
+import WidgetGroupLoader from '@/modules/reports/components/Widget/Group/Loader.vue'
 import WidgetCondensed from '@/modules/reports/components/Widget/Condensed/Condensed.vue'
 </script>
 
@@ -8,8 +9,12 @@ import WidgetCondensed from '@/modules/reports/components/Widget/Condensed/Conde
   <PublicHeadLine />
 
   <WidgetGroup>
-    <WidgetCondensed :value="10" label="Gists total" />
-    <WidgetCondensed :value="5" label="Gists gratuitos" />
-    <WidgetCondensed :value="5" label="Gists pagos" />
+    <WidgetGroupLoader :loading="false" :amount="3">
+      <WidgetCondensed :value="10" label="Gists total" />
+      <WidgetCondensed :value="5" label="Gists gratuitos" />
+      <WidgetCondensed :value="5" label="Gists pagos"
+    /></WidgetGroupLoader>
   </WidgetGroup>
+
+  <WidgetDefault title="Todos os gists"> gists </WidgetDefault>
 </template>
